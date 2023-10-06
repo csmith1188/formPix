296b2856e37c2c826dc22aea8d918e1aef14003c
Ryan24313 <ryan24313@live.ytech.edu>
2023-10-06 14:55:25 -0400

    added config file

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const ws281x = require('rpi-ws281x-native')
+const ws281x = require('rpi-ws281x-native') // this changes the pixels
 const { io } = require('socket.io-client')
-const sharp = require('sharp')
+const sharp = require('sharp') // this converts text to an image
+const fs = require('fs')
 
-const classCode = 'd5f5'
-const brightness = 100
-const api = 'f5f91296026fcc3880c548bf88ca470f9f281fb1562596996e311eb29394e218443d561aa92a886f39eb40da5b254135fc0eb302c6fa6df87705af2f0bdf6108'
-// school
-const ip = 'http://172.16.3.103:420'
-const maxPixels = 12
-const gpio = 21
-const stripType = ws281x.stripType.WS2811_RGB
-const HEIGHT = 4
-const WIDTH = 4
-// home
-// const ip = 'http://192.168.0.8:420'
-// const maxPixels = 52
-// const gpio = 18
-// const stripType = ws281x.stripType.SK6812_GRBW
+// config
+const config = JSON.parse(
+	fs.readFileSync('settings.json')
+)
 
+const {
+	ip,
+	api,
+	classCode,
+	brightness,
+	pin,
+	barPixels,
+	stripType
+} = config
+const { boardHeight, boardWidth } = config.board
+
+const maxPixels = barPixels + boardHeight * boardWidth
+
+// set up strip settings
 let strip = ws281x(maxPixels, {
 	dma: 10,
 	freq: 800000,
-	gpio: gpio,
+	gpio: pin,
 	invert: false,
 	brightness: brightness,
 	stripType: stripType
@@ -29,6 +33,7 @@ let strip = ws281x(maxPixels, {
 let pixels = strip.array
 
 // fill strip with color
+// by default start is 0 and length is the whole strip
 function fill(color, start = 0, length = pixels.length) {
 	if (length >= pixels.length) length = pixels.length
 
@@ -37,63 +42,134 @@ function fill(color, start = 0, length = pixels.length) {
 	}
 }
 
-async function stringToImage(string, textColor, backgroundColor) {
-	let safeString = ''
-	let file = ''
+// skips every x item in an array
+function skipNumber(array, skipAmount) {
+	let result = []
 
-	string = string.toUpperCase()
-
-	for (let character of string) {
-		safeString += `&#${character.charCodeAt(0)};`
-
-		if (file) file += '-'
-		file += character.charCodeAt(0)
+	for (let i = 0; i < array.length; i += skipAmount) {
+		result.push(array[i])
 	}
 
-	if (file.length >= 50) file = string.length
+	return result
+}
 
-	let pixels = [[]]
+// This function converts a 1D array into a 2D array with a specified height and width
+function convertTo2DArray(oneDArray, height, width) {
+	// Initialize a 2D array with the specified height and width, filled with 0s
+	let twoDArray = new Array(height).fill(null).map(() => new Array(width).fill(0))
+	// Initialize an index to keep track of the current element in the 1D array
+	let index = 0
+
+	// Iterate over the columns of the 2D array
+	for (let col = 0; col < width; col++) {
+		// Iterate over the rows of the 2D array
+		for (let row = 0; row < height; row++) {
+			// If the current index is within the bounds of the 1D array,
+			if (index < oneDArray.length) {
+				// assign the current element to the current position in the 2D array
+				twoDArray[row][col] = oneDArray[index]
+				// increment the index
+				index++
+			} else {
+				// If the current index is out of bounds of the 1D array,
+				// assign null to the current position in the 2D array
+				twoDArray[row][col] = null
+			}
+		}
+	}
+
+	// Return the 2D array
+	return twoDArray
+}
+
+// This function reverses the order of elements in each odd-indexed row of a 2D array
+function reverse2DArray(twoDArray) {
+	// Iterate over each row in the 2D array
+	for (let row = 0; row < twoDArray.length; row++) {
+		// If the row index is odd (i.e., row is an odd-indexed row)
+		if (row % 2 == 1) {
+			// Reverse the order of elements in the row
+			twoDArray[row].reverse()
+		}
+	}
+	// Return the modified 2D array
+	return twoDArray
+}
+
+// This function transposes a 2D array, i.e., it swaps its rows and columns
+function transposeArray(array, height, width) {
+	// Initialize a new 2D array filled with 0s, with dimensions (width x height)
+	let transposedArray = Array.from({ length: width }, () => Array(height).fill(0))
+	// Iterate over each row in the original 2D array
+	for (let row = 0; row < height; row++) {
+		// Iterate over each column in the original 2D array
+		for (let col = 0; col < width; col++) {
+			// Copy the element from the original 2D array to the transposed 2D array
+			// but in a transposed manner, i.e., array[row][col] becomes transposedArray[col][row]
+			transposedArray[col][row] = array[row][col]
+		}
+	}
+	// Return the transposed 2D array
+	return transposedArray
+}
+
+// convert string to image
+async function displayBoard(string, textColor, backgroundColor) {
+	let boardPixels = []
+
 	try {
+		// convert the string to an image
 		let image = await sharp({
 			text: {
-				text: safeString,
+				text: string,
 				font: 'Consolas',
-				height: HEIGHT,
-				width: WIDTH,
+				height: boardHeight,
+				width: boardWidth,
 			}
-		}).threshold()
+		})
+			.rotate(-90)
+			// .flip() // y-axis
+			.threshold() // sharpen image
+		let metadata = await image.metadata()
+		image.toFile(`./test.png`) // test
 
-		// image.toFile(`./images/${file}.png`)
-
+		// get bytes of image
 		let buffer = await image.toBuffer()
 
 		let x = 0
 		let y = 0
+		fs.writeFileSync('test.json', JSON.stringify({
+			metadata: metadata,
+			buffer: buffer
+		}))
 
-		for (let i = 0; i < buffer.length; i += 3) {
-			if (x == image.options.input.textWidth) {
-				x = 0
-				y++
-				pixels[y] = []
-			}
+		boardPixels = skipNumber(buffer, 3)
+		boardPixels = convertTo2DArray(boardPixels, boardHeight, boardWidth)
+		boardPixels = transposeArray(boardPixels, boardHeight, boardWidth)
+		boardPixels = reverse2DArray(boardPixels)
+		boardPixels = boardPixels.flat()
 
-			if (buffer[i] == 255)
-				pixels[y][x] = textColor
-			else
-				pixels[y][x] = backgroundColor
-			x++
+		for (let pixel = 0; pixel < boardPixels.length; pixel++) {
+			if (boardPixels[pixel] == 255) boardPixels[pixel] = textColor
+			else if (boardPixels[pixel] == 0) boardPixels[pixel] = backgroundColor
+			else boardPixels[pixel] = 0x000000
 		}
 
-		return pixels
+		for (let currentPixel = 0; currentPixel < boardPixels.length; currentPixel++) {
+			pixels[currentPixel + barPixels + 8] = boardPixels[currentPixel]
+		}
+		ws281x.render()
+
 	} catch (error) {
-		console.log(string.charCodeAt(0), string, error)
+		console.log(string, error)
 	}
 }
 
-// clear strip
+// clear pixels
 fill(0x000000)
 ws281x.render()
 
+// set web socket url
 const socket = io(ip, {
 	query: {
 		api: api,
@@ -101,8 +177,9 @@ const socket = io(ip, {
 	}
 })
 
+// when there is a connection error it tys to reconnect
 socket.on('connect_error', (err) => {
-	console.log(err.message)
+	console.log('connection error')
 
 	fill(0x000000)
 	ws281x.render()
@@ -112,48 +189,42 @@ socket.on('connect_error', (err) => {
 	}, 5000)
 })
 
+// when it connects to formBar it ask for the bars data
 socket.on('connect', () => {
 	console.log('connected')
 	socket.emit('vbUpdate')
 })
 
+// when the bar changes
 socket.on('vbUpdate', (pollsData) => {
-	console.log('vbUpdate', pollsData)
 	let pixelsPerStudent
 
+	// if no poll clear pixels
 	if (!pollsData.pollStatus) {
 		fill(0x000000)
 		ws281x.render()
 		return
 	}
 
-	fill(0x808080)
+	fill(0x808080, 0, barPixels)
+	fill(0x000000, barPixels, boardHeight * boardWidth)
 
-	// convert colors to integers
+	// convert colors from hex to integers
 	for (let pollData of Object.values(pollsData.polls)) {
 		pollData.color = parseInt(pollData.color.slice(1), 16)
 	}
 
 	let pollResponses = 0
 
+	// count poll responses
 	for (let poll of Object.values(pollsData.polls)) {
 		pollResponses += poll.responses
 	}
 
-	if (pollsData.totalStudents == pollResponses) pollsData.blindPoll = false
-
-	if (pollsData.blindPoll) {
-		if (pollsData.totalStudents <= 0) pixelsPerStudent = 0
-		else pixelsPerStudent = Math.floor((pixels.length - 1) / pollsData.totalStudents)
-
-		fill(
-			0xFFAA00,
-			0,
-			pixelsPerStudent * pollResponses
-		)
-
-		ws281x.render()
-		return
+	// if totalStudents = pollResponses turn off blind mode
+	if (pollsData.totalStudents == pollResponses) {
+		pollsData.blindPoll = false
+		displayBoard('POGS', 0xFF0000, 0x0000FF)
 	}
 
 	// count non-empty polls
@@ -163,23 +234,44 @@ socket.on('vbUpdate', (pollsData) => {
 			nonEmptyPolls++
 		}
 	}
+
 	if (pollsData.totalStudents <= 0) pixelsPerStudent = 0
-	else pixelsPerStudent = Math.floor((pixels.length - nonEmptyPolls) / pollsData.totalStudents)
+	else pixelsPerStudent = Math.floor((barPixels - nonEmptyPolls) / pollsData.totalStudents) //- nonEmptyPolls
 
 	// add polls
 	let currentPixel = 0
-	for (let [name, poll] of Object.entries(pollsData.polls)) {
-		let length = pixelsPerStudent * poll.responses
+	let pollNumber = 0
 
-		if (length > 0)
+	for (let [name, poll] of Object.entries(pollsData.polls)) {
+		// for each response
+		for (let responseNumber = 0; responseNumber < poll.responses; responseNumber++) {
+			let color = poll.color
+			if (pollsData.blindPoll) color = 0xFF8000
+
+			// set response to color
 			fill(
-				poll.color,
+				color,
 				currentPixel,
-				length
+				pixelsPerStudent
 			)
 
-		if (poll.responses > 0) currentPixel++
-		currentPixel += length
+			currentPixel += pixelsPerStudent
+
+			// set spacers
+			if (
+				responseNumber < poll.responses - 1 ||
+				pollNumber < nonEmptyPolls
+			) {
+				pixels[currentPixel] = 0xFF0080
+				currentPixel++
+			}
+		}
+
+		if (
+			!pollsData.blindPoll &&
+			poll.responses > 0
+		) currentPixel++
+		pollNumber++
 	}
 
 	ws281x.render()
